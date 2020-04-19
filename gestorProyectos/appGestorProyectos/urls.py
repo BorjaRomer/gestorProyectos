89c7b0a75249eba1e9500ef342cfb9690ff3d449
Borja <borja.romero@opendeusto.es>
2020-04-19 17:52:08 +0200

    Login

--- gestorProyectos/appGestorProyectos/urls.py
+++ gestorProyectos/appGestorProyectos/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     #HOME
     path('home/', views.ProyectosListView.as_view(), name='home'),
+    path('login/', views.LoginView.as_view(), name='login'),
     #PROYECTOS
     path('nuevo_proyecto/', views.CrearProyectoView.as_view(), name='proyecto_form'),
     path('proyectos/', views.ProyectoListView.as_view(), name='proyecto_list'),
