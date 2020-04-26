5f256198e5308f58412557d9a18ffcd8d8be9656
AitorContrasta <aitor.contrasta@opendeusto.es>
2020-04-26 11:46:22 +0200

    Boton menú pequeño

--- gestorProyectos/appGestorProyectos/urls.py
+++ gestorProyectos/appGestorProyectos/urls.py
@@ -2,22 +2,22 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #HOME
+    # HOME
     path('', views.ProyectosListView.as_view(), name='home'),
-    #PROYECTOS
+    # PROYECTOS
     path('nuevo_proyecto/', views.ProyectoCreateView.as_view(), name='proyecto_form'),
     path('proyectos/', views.ProyectoListView.as_view(), name='proyecto_list'),
     path('proyecto/<int:pk>/', views.ProyectoDetailView.as_view(), name='proyecto'),
     path('proyecto/<int:pk>/delete/', views.ProyectoDeleteView.as_view(), name='proyecto_delete'),
     path('proyecto/<int:pk>/update/', views.ProyectoUpdateView.as_view(), name='proyecto_update'),
     path('proyecto/<int:pk>/cliente/', views.ClienteDetailView.as_view(), name='cliente'),
-    #TAREAS
+    # TAREAS
     path('nueva_tarea/', views.TareaCreateView.as_view(), name='tarea_form'),
     path('tareas/', views.TareaListView.as_view(), name='tarea_list'),
     path('tarea/<int:pk>/', views.TareaDetailView.as_view(), name='tarea'),
     path('tarea/<int:pk>/delete/', views.TareaDeleteView.as_view(), name='tarea_delete'),
     path('tarea/<int:pk>/update/', views.TareaUpdateView.as_view(), name='tarea_update'),
-    #EMPLEADOS
+    # EMPLEADOS
     path('nuevo_empleado/', views.EmpleadoCreateView.as_view(), name='empleado_form'),
     path('empleados/', views.EmpleadoListView.as_view(), name='empleado_list'),
     path('empleado/<int:pk>/', views.EmpleadoDetailView.as_view(), name='empleado'),
